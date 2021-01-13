c1a0353d97ee8197314319e53166f8d171600ff3
vishwas1 <vishwasanand.cse@gmail.com>
2021-01-13 12:08:18 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -48,9 +48,9 @@ app.use(function (req, res, next) {
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // // Set to true if you need the website to include cookies in the requests sent
+  // // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
