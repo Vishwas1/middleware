a9fe3fc579b375b3d5e40febde55e674cf8783ed
vishwas1 <vishwasanand.cse@gmail.com>
2021-01-13 12:06:37 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,27 @@ export default async function app() {
     app.use(bodyParser.json());
     app.use(express.static('public'));
 
+
+    // Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
     // Implement /hs/api/v2/auth API 
     app.post('/hs/api/v2/auth', hypersign.authenticate.bind(hypersign), async (req, res) => {
         try {
